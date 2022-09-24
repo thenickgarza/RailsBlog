fa23cd8917e13b1234495d32134ccaeb84b350cb
Nick Garza <thenickgarza14@gmail.com>
2022-09-23 19:23:39 -0500

    added CRUD a comment to a article

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
   # Defines the root path route ("/")
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end 
 
 end
